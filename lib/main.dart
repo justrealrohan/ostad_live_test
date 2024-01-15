da54c027f15abe169c0bdf5bbe10f3cba261d87f
rohansmac <rohan@webit.life>
2024-01-15 17:05:12 +0600

    Initial Commit v0.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,30 @@
-/// Hello
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeActivity(),
+    );
+  }
+}
+
+class HomeActivity extends StatelessWidget {
+  const HomeActivity({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text('Home Activity'),
+      ),
+    );
+  }
+}
